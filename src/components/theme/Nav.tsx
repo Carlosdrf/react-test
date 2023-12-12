7c610a9a8eba86f859c0f3afd9609e5549675273
Carlos <cdavidrivas@hotmail.com>
2023-12-12 08:26:22 -0400

    navigation component

--- src/components/theme/Nav.tsx
+++ src/components/theme/Nav.tsx
@@ -1,61 +1,57 @@
 export const Nav = () => {
   return (
     <>
-    
-      <nav className="sticky-top navbar-dark bg-dark">
+      <nav className="sticky-top navbar-dark bg-dark navbar navbar-expand-lg">
         <div className="container">
-          <div className="navbar">
-            <span className="brand">
-              <a href="index.php" className="navlink active">
-                THENOSWEB
-              </a>
-            </span>
-            <div>
-              <button
-                className="nav-btn"
-                id="nav-btn"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="nav-icon">Button</span>
-              </button>
-            </div>
-            <div className="collapse" id="collapse">
-              <ul>
-                <li className="navitem">
-                  <a className="navlink" href="entrenamientos.php">
-                    {" "}
-                    Entrenamientos
-                  </a>
-                </li>
-                <li className="navitem">
-                  <a className="navlink" href="alimentacion.php">
-                    {" "}
-                    Alimentacion
-                  </a>
-                </li>
-                <li className="navitem">
-                  <a className="navlink" href="calculadora.php">
-                    {" "}
-                    Calculadoras
-                  </a>
-                </li>
-                <li className="separation"></li>
-                {/* <div className="dropdown">
-                                <button className="navlink active loginnav drop-button" id="drop-button"></button>
+          <span className="navbar-brand">
+            <a href="index.php" className="nav-link active">
+              THENOSWEB
+            </a>
+          </span>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="entrenamientos.php">
+                  Entrenamientos
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="alimentacion.php">
+                  {" "}
+                  Alimentacion
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="calculadora.php">
+                  {" "}
+                  Calculadoras
+                </a>
+              </li>
+              <li className="separation"></li>
+              {/* <div className="dropdown">
+                                <button className="nav-link active loginnav drop-button" id="drop-button"></button>
                                 <div id="mydropdown" className="accountoption">
                                     <a href="account.php">Cuenta</a>
                                     <div className="dropdown-divider"></div>
                                     <a href="php/logout.php">Cerrar sesión</a>
                                 </div>
                             </div>
-                            <a className="navlink active loginnav" href="Sign_Up.php"> Iniciar sesión</a> */}
-              </ul>
-            </div>
+                            <a className="nav-link active loginnav" href="Sign_Up.php"> Iniciar sesión</a> */}
+            </ul>
           </div>
         </div>
       </nav>
